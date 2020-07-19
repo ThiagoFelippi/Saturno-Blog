eb85f8d9ba2c5289aa3c66777bf871e40d32dc23
Thiago Felippi <thiagocrespo241103@gmail.com>
2020-07-19 18:15:58 -0300

    refactor(user entity): change user entity for be able to make methods in resolver

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,17 +1,25 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm'
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity } from 'typeorm'
+import { ObjectType, Field, Int } from 'type-graphql'
+import { Min } from 'class-validator'
 
+@ObjectType()
 @Entity()
-export class User{
+export class User extends BaseEntity{
 
+  @Field(() => Int)
+  @Min(0)
   @PrimaryGeneratedColumn()
   id: number
 
+  @Field()
   @Column()
   name : string
 
+  @Field()
   @Column()
   email : string
 
+  @Field()
   @Column()
   password : string
 
